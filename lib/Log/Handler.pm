fb08370c9574feec93d1e1fc6d065b889e3bbffa
Jonny Schulz <jschulz@bloonix.de>
2012-03-08 06:17:44 +0000

    Import of BLOONIX/Log-Handler-0.74 from CPAN.
    gitpan-cpan-distribution: Log-Handler
    gitpan-cpan-version:      0.74
    gitpan-cpan-path:         BLOONIX/Log-Handler-0.74.tar.gz
    gitpan-cpan-author:       BLOONIX
    gitpan-cpan-maturity:     released

--- lib/Log/Handler.pm
+++ lib/Log/Handler.pm
@@ -1059,7 +1059,7 @@ use Log::Handler::Pattern;
 use UNIVERSAL;
 use base qw(Log::Handler::Levels);
 
-our $VERSION = "0.73";
+our $VERSION = "0.74";
 our $ERRSTR  = "";
 
 # $TRACE and $CALLER_LEVEL are both used as global
